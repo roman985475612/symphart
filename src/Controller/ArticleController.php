13151c5e5688b1fe7fa4d205fff91d2f06199df4
Roman <roman985475612@gmail.com>
2020-09-14 14:13:31 +0300

    Chapter 3 completed

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,22 +3,89 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+//use http\Env\Request;
+use Symfony\Component\Form\Extension\Core\Type\{TextType, TextareaType, SubmitType};
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class ArticleController extends AbstractController {
     /**
      * @Route("/", name="article_list")
-     * @Method({"GET"})
+     * Method({"GET"})
      */
     public function index() {
         $articles = $this->getDoctrine()->getRepository(Article::class)->findAll();
 
         return $this->render('articles/index.html.twig', [
-            'name' => 'Роман',
             'articles' => $articles,
+        ]);
+    }
+
+    /**
+     * @Route("/article/new", name="article_new")
+     * Method({"GET", "POST"})
+     */
+    public function new(Request $request) {
+        $article = new Article();
+
+        $form = $this->createFormBuilder($article)
+            ->add('title', TextType::class, ['attr' => ['class' => 'form-control']])
+            ->add('body', TextareaType::class, [
+                'required' => false,
+                'attr' => ['class' => 'form-control']])
+            ->add('save', SubmitType::class, [
+                'label' => 'Создать',
+                'attr' => ['class' => 'btn btn-primary mt-3']])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $article = $form->getData();
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($article);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('article_list');
+        }
+
+        return $this->render('articles/new.html.twig', [
+           'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/article/{id}/update", name="article_update")
+     * Method({"GET", "POST"})
+     */
+    public function update(Request $request, $id) {
+        $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+
+        $form = $this->createFormBuilder($article)
+            ->add('title', TextType::class, ['attr' => ['class' => 'form-control']])
+            ->add('body', TextareaType::class, [
+                'required' => false,
+                'attr' => ['class' => 'form-control']])
+            ->add('save', SubmitType::class, [
+                'label' => 'Создать',
+                'attr' => ['class' => 'btn btn-primary mt-3']])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->flush();
+
+            return $this->redirectToRoute('article_list');
+        }
+
+        return $this->render('articles/update.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 
@@ -32,18 +99,16 @@ class ArticleController extends AbstractController {
     }
 
     /**
-     * @Route("/article/save")
+     * @Route("/article/{id}/delete", name="article_delete")
+     * Method({"DELETE"})
      */
-    public function save() {
+    public function delete(Request $request, $id) {
+        $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+
         $entityManager = $this->getDoctrine()->getManager();
-
-        $article = new Article();
-        $article->setTitle('Статья 2');
-        $article->setBody('Текст статьи 2');
-
-        $entityManager->persist($article);
+        $entityManager->remove($article);
         $entityManager->flush();
 
-        return new Response('Saves an article with the id of '.$article->getId());
+        return $this->redirectToRoute('article_list');
     }
 }
